19cbb393f12605ffa1c9d13e2db0ec24a0b73d15
kazamok <162867216+kazamok@users.noreply.github.com>
2025-05-22 23:06:32 +0900

    Update mod-iplimit-manager.sql

--- data/sql/db-world/mod-iplimit-manager.sql
+++ data/sql/db-world/mod-iplimit-manager.sql
@@ -1,5 +1,7 @@
 -- 허용된 IP 주소를 저장하는 테이블 생성
-CREATE TABLE IF NOT EXISTS `custom_allowed_ips` (
-  `ip` VARCHAR(45) NOT NULL,
+
+CREATE TABLE IF NOT EXISTS `acore_world.custom_allowed_ips` (
+  `ip` varchar(45) NOT NULL,
+  `text` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci,
   PRIMARY KEY (`ip`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
